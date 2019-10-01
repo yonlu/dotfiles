572b90942f32d03ab1b011458f92bd94a4153858
yonlu <lss.emailbox@gmail.com>
2019-10-01 09:46:26 -0400

    fix: removed relative paths

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -1,1 +1,21 @@
-/home/luke/.config/polybar/launch.sh
+#!/usr/bin/env bash
+
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+# Launch example bar
+polybar example
+
+# Config for multiple monitors
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload example &
+  done
+else
+  polybar --reload example &
+fi
+
+echo "Bars launched..."
