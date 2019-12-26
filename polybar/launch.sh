af47443d9e3bdb7f77246ade12150b8e12697637
yonlu <lss.emailbox@gmail.com>
2019-12-26 10:46:16 -0500

    chore: fixed polybar's bar names

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -7,15 +7,15 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch example bar
-polybar example
+polybar primary
 
 # Config for multiple monitors
 if type "xrandr"; then
   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar --reload example &
+    MONITOR=$m polybar --reload primary &
   done
 else
-  polybar --reload example &
+  polybar --reload primary &
 fi
 
 echo "Bars launched..."
